526ebabc18a9be34876524546c0464ca88e01053
Sujan <sujan.sooriyakumar@gmail.com>
2021-04-16 18:21:49 -0400

    Basic character movement complete
    Character has the ability to walk, run, and jump in multiple directions

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,38 +4,111 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    CharacterController controller;
+    Animator animator;
     public Transform cam;
     public float speed = 6f;
     float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+    public bool groundedPlayer;
+    private float jumpHeight = 8.0f;
+    Vector3 direction;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
 
     private void Update()
     {
-        // Get vector for player movement
+
+        /*
+         * Calculate player movement and rotation
+         */
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (controller.isGrounded)
+        {
+            direction = new Vector3(horizontal, 0, vertical).normalized;
+            /*
+             * Jumping
+             */
+            if (Input.GetButton("Jump"))
+            {
+                animator.SetTrigger("Jump");
+            }
 
-        if(direction.magnitude >= 0.1f)
+       
+
+
+        }
+        if (direction.magnitude >= 0.1f)
         {
             // Adjust player rotation
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
+            
+        }
+      direction.y += gravityValue * Time.deltaTime;
             // Move the player
             controller.Move(direction * speed * Time.deltaTime);
-        }
+            groundedPlayer = controller.isGrounded;
 
-        // Walk animation
-        if(horizontal != 0 || vertical != 0)
+
+        UpdateAnimations();
+
+    }
+
+    public void Jump()
+    {
+        direction.y = jumpHeight;
+        animator.ResetTrigger("Jump");
+    }
+
+    void UpdateAnimations()
+    {
+        /*
+       * Walk animation
+       */
+
+        if (direction.x != 0 || direction.z != 0)
         {
-            GetComponent<Animator>().SetBool("Walking", true);
+            animator.SetBool("Walking", true);
         }
         else
         {
-            GetComponent<Animator>().SetBool("Walking", false);
+            animator.SetBool("Walking", false);
+        }
+
+        /*
+        * Run animation
+        */
+
+        if (Input.GetButton("Run"))
+        {
+            speed = 8.0f;
+            animator.SetBool("Run", true);
+
+        }
+        if (Input.GetButtonUp("Run"))
+        {
+            speed = 6.0f;
+            animator.SetBool("Run", false);
+        }
+
+        if (controller.isGrounded)
+        {
+            animator.SetBool("Grounded", true);
+
+        }
+
+        else
+        {
+            animator.SetBool("Grounded", false);
         }
     }
 }
